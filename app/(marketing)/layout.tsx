5e063eacbdc9904456cb1fde5f4801d80a976648
LeeDat03 <leedat104517@gmail.com>
2024-04-06 21:51:57 +0700

    add landing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import { NavBar } from "./_components/navbar";
+
+export default function MarketingLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="h-full dark:bg-[#1F1F1F]">
+      <NavBar />
+      <main className="h-full pt-40">{children}</main>
+    </div>
+  );
+}
